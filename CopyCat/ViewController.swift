5ea8de9a62806acc698eebae43acbb5f2ff7160e
Garth <liuyandong@gmail.com>
2016-09-11 09:15:59 +0800

    Initial Commit

---
+++ CopyCat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CopyCat
+//
+//  Created by YAN DONG LIU on 9/11/16.
+//  Copyright © 2016 YAN DONG LIU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
